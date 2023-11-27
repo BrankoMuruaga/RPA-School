8a239f95af71fe83af44eb7674baf1c58b50379a
Branko Muruaga <muruagabranko253@gmail.com>
2023-11-27 09:45:29 -0300

    fx env

--- src/Data/DATA-Users.jsx
+++
@@ -1,2 +0,0 @@
-export const VITE_USERS_DATA =
-  '{"trainee": {"password": "passwordTrainee"}, "junior": {"password": "passwordJunior"}, "semi_senior": {"password": "passwordSemiSenior"}, "senior": {"password": "passwordSenior"}, "senior_experienced": {"password": "passwordSeniorExperienced"}}';
